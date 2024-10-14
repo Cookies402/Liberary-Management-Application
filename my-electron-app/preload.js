0230c2e314097032aeea9d0e1ef13311f9f3d1ee
Cookies402 <Cookies402@proton.me>
2024-10-14 23:21:19 +0530

    Almost done

--- my-electron-app/preload.js
+++ my-electron-app/preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('ipcRenderer', {
-    invoke: (channel, data) => ipcRenderer.invoke(channel, data)
+    invoke: (channel, data) => ipcRenderer.invoke(channel, data).catch(err => console.error(`Error in ${channel} IPC call:`, err))
 });
 
 window.addEventListener('DOMContentLoaded', () => {
